957632a9048a0e6d061d66aae1ac8f04f7bfdaa5
Ben Golden <bengolden1@gmail.com>
2014-07-22 17:00:54 -0500

    all hand checking methods complete

--- botspec.rb
+++ botspec.rb
@@ -1,32 +1,43 @@
 require_relative "poker_bot"
 
 class PokerHand
-
 	describe 'evaluate hands' do
 
-	let(:card1) {Card.new(2,"s")}
-	let(:card2) {Card.new(3,"s")}
-	let(:card3) {Card.new(4,"s")}
-	let(:card4) {Card.new(5,"d")}
-	let(:card5) {Card.new(6,"s")}
-	let(:card6) {Card.new(8,"s")}
+	let(:s2) {Card.new(2,"s")}
+	let(:s3) {Card.new(3,"s")}
+	let(:s4) {Card.new(4,"s")}
+	let(:s5) {Card.new(5,"s")}
+	let(:d5) {Card.new(5,"d")}
+	let(:h5) {Card.new(5,"h")}
+	let(:c5) {Card.new(5,"c")}
+
+	let(:s6) {Card.new(6,"s")}
+	let(:c9) {Card.new(9,"c")}
+
+	let(:sa) {Card.new(14,"s")}
+	let(:ca) {Card.new(14,"a")}
 
 		describe '.straight?' do
+			let(:hand1) {PokerHand.new([s2,s3,s4,d5,s6,h5])}
+			let(:hand2) {PokerHand.new([s4,s6,s2,d5,s3])}
+			let(:wheel) {PokerHand.new([s2,s3,d5,c9,sa,s4])}
+			let(:hand3) {PokerHand.new([s2,s3,sa,d5,s6])}
+			let(:hand4) {PokerHand.new([s2,s3,s4,d5])}
 
-			let(:hand1) {PokerHand.new([card1,card2,card3,card4,card5])}
-			let(:hand2) {PokerHand.new([card1,card2,card4,card5,card6])}
-			let(:hand3) {PokerHand.new([card3,card5,card4,card1,card2])}
-			let(:hand4) {PokerHand.new([card3,card5,card4,card1])}
 			it 'returns true if hand is a straight' do
 				expect(hand1.straight?).to equal(true)
 			end
 
-			it 'returns false if hand is not a straight' do
-				expect(hand2.straight?).to equal(false)
+			it 'returns true if straight is out of order' do
+				expect(hand2.straight?).to equal(true)
 			end
 
-			it 'returns true if straight is out of order' do
-				expect(hand3.straight?).to equal(true)
+			it 'returns true if wheel' do
+				expect(wheel.straight?).to equal(true)
+			end
+
+			it 'returns false if hand is not a straight' do
+				expect(hand3.straight?).to equal(false)
 			end
 
 			it 'returns false if invalid hand' do
@@ -35,8 +46,8 @@ class PokerHand
 		end
 
 		describe '.flush?' do
-			let(:hand1) {PokerHand.new([card1,card2,card3,card5,card6])}
-			let(:hand2) {PokerHand.new([card1,card2,card4,card5,card6])}
+			let(:hand1) {PokerHand.new([s2,s3,s4,s6,sa])}
+			let(:hand2) {PokerHand.new([s2,d5,s4,s6,sa])}
 
 			it 'returns true if hand is a flush' do
 				expect(hand1.flush?).to equal(true)
@@ -48,10 +59,9 @@ class PokerHand
 		end		
 
 		describe '.straight_flush?' do
-			let(:card7) {Card.new(5,"s")}
-			let(:hand1) {PokerHand.new([card1,card2,card3,card7,card5])}
-			let(:hand2) {PokerHand.new([card1,card2,card3,card5,card6])}
-			let(:hand3) {PokerHand.new([card3,card5,card4,card1,card2])}
+			let(:hand1) {PokerHand.new([s2,s3,s4,s6,s5])}
+			let(:hand2) {PokerHand.new([s2,s3,s4,s6,sa])}
+			let(:hand3) {PokerHand.new([s4,s6,s2,d5,s3])}
 
 			it 'returns true if straight and flush' do
 				expect(hand1.straight_flush?).to equal(true)
@@ -65,6 +75,65 @@ class PokerHand
 				expect(hand3.straight_flush?).to equal(false)
 			end
 		end
-	end
 
+		describe '.full_house?' do
+			let(:hand1) {PokerHand.new([s5,c5,h5,sa,ca])}
+			let(:hand2) {PokerHand.new([s5,c5,h5,sa,d5])}
+			it 'returns true if full house' do
+				expect(hand1.full_house?).to equal(true)
+			end
+
+			it 'returns false if not full house' do
+				expect(hand2.full_house?).to equal(false)
+			end
+		end
+
+		describe '.quads?' do
+			let(:hand1) {PokerHand.new([s5,c5,h5,sa,d5])}
+			let(:hand2) {PokerHand.new([s5,c5,h5,sa,ca])}
+			it 'returns true if full house' do
+				expect(hand1.quads?).to equal(true)
+			end
+
+			it 'returns false if not full house' do
+				expect(hand2.quads?).to equal(false)
+			end
+		end
+
+		describe '.trips?' do
+			let(:hand1) {PokerHand.new([s3,c5,h5,sa,d5])}
+			let(:hand2) {PokerHand.new([s3,c5,h5,sa,ca])}
+			it 'returns true if trips' do
+				expect(hand1.trips?).to equal(true)
+			end
+
+			it 'returns false if not trips' do
+				expect(hand2.trips?).to equal(false)
+			end
+		end
+
+		describe '.two_pair?' do
+			let(:hand1) {PokerHand.new([s3,c5,h5,sa,ca])}
+			let(:hand2) {PokerHand.new([s3,c5,h5,sa,d5])}
+			it 'returns true if two pair' do
+				expect(hand1.two_pair?).to equal(true)
+			end
+
+			it 'returns false if not two pair' do
+				expect(hand2.two_pair?).to equal(false)
+			end
+		end
+
+		describe '.pair?' do
+			let(:hand1) {PokerHand.new([s3,s2,h5,sa,ca])}
+			let(:hand2) {PokerHand.new([s3,s2,s4,sa,d5])}
+			it 'returns true if two pair' do
+				expect(hand1.pair?).to equal(true)
+			end
+
+			it 'returns false if not two pair' do
+				expect(hand2.pair?).to equal(false)
+			end
+		end
+	end
 end
--- poker_bot.rb
+++ poker_bot.rb
@@ -1,29 +1,57 @@
 class PokerHand
 	def initialize(hand)
-		@hand = hand
+		@hand = hand.sort_by{|card|card.num}
 	end
 
 	def straight?
-		return false if invalid_hand?
-		@hand.sort_by!{|card|card.num}
-		(1..4).all? do |index|
-			@hand[0].num == @hand[index].num - index
+		return false if values_present.length < 5
+		values_present.each_cons(5).any? do |five_card_hand|
+			(1..4).all? do |index|
+				five_card_hand[index] - five_card_hand[0] == index
+			end
 		end
 	end
 
 	def flush?
-		return false if invalid_hand?
-		@hand.all? do |card|
-			@hand[0].suit == card.suit
-		end
+		suit_counts[0] >= 5
 	end
 
 	def straight_flush?
 		straight? && flush?
 	end
 
-	def invalid_hand?
-		@hand.length != 5
+	def full_house?
+		value_counts[0] == 3 && value_counts[1] == 2
+	end
+
+	def two_pair?
+		value_counts[0] == 2 && value_counts[1] == 2
+	end
+
+	def quads?
+		value_counts[0] == 4
+	end
+
+	def trips?
+		value_counts[0] == 3
+	end
+
+	def pair?
+		value_counts[0] == 2
+	end
+
+	def value_counts
+		@hand.group_by{|card| card.num}.values.map{|cards|cards.count}.sort.reverse
+	end
+
+	def values_present
+		output = @hand.map{|card|card.num}.uniq
+		output = [1] + output if output.last == 14
+		output
+	end
+
+	def suit_counts
+		@hand.group_by{|card| card.suit}.values.map{|cards|cards.count}.sort.reverse
 	end
 
 end
